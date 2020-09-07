bf6e5553422a7219b34a956e9b426220ad1ed156
LSmith-Zenoscave <lsmith@zenoscave.com>
2020-09-07 11:46:36 -0600

    created base orm and orm testing
    Signed-off-by: LSmith-Zenoscave <lsmith@zenoscave.com>

---
+++ glow/model/__init__.py
@@ -0,0 +1,2 @@
+from glow.model.base import Base
+from glow.model.url import UrlModel as Url
--- tests/test_url_server.py
+++ tests/test_url_server.py
@@ -1,14 +1,15 @@
 from urllib.parse import ParseResult
+from glow.model import Url
 
 
 class TestUrlServer:
-    url_server_scenarios = (
-        ["http://google.com"],
-        ["https://google.com", "https://reddit.com"],
-    )
+    def test_insert_urls(self, url_server):
+        url = url_server.insert_url(host="facebook.com", path="me")
+        assert isinstance(url, Url)
 
     def test_get_urls(self, url_server):
+        url = url_server.insert_url(host="google.com", path="/")
         urls = url_server.list_urls()
         assert isinstance(urls, list)
-        assert all(isinstance(url, dict) for url in urls)
-        assert all(isinstance(url["url"], ParseResult) for url in urls)
+        assert all(isinstance(url, Url) for url in urls)
+        assert url in urls
